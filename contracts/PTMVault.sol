0e2ff65e19b1800ffab879410f30215d7e78482c
bornbig <72680443+bornbig@users.noreply.github.com>
2021-05-22 22:30:32 +0530

    Added liquidity and remove liquidity options

--- contracts/PTMVault.sol
+++ contracts/PTMVault.sol
@@ -9,14 +9,20 @@ import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol"
 contract PTMVault is  ERC20, ERC20PresetMinterPauser{
     using SafeMath for uint;
 
+    // Defining tokens for the swap
+    IERC20 tokenA;
+    IERC20 tokenB;
+
     address public factory;
+    address public swap;
 
-    constructor() ERC20PresetMinterPauser("PTMVault", "PTM-V"){}
+    constructor(address _tokenA, address _tokenB) ERC20PresetMinterPauser("PTMVault", "PTM-V"){
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+    }
 
-    addLiquidityToPool(address tokenA, address tokenB, uint amount0, uint amount1){
-        PTMFactory factory = PTMFactory(factory);
+    function addLiquidityToPool(uint amount0, uint amount1){
         
-        address swap = factory.pairs[tokenA][tokenB];
         PTMSwap ptmSwap = PTMSwap(swap);
 
         (uint reserve1, uint resreve2) = ptmSwap.getReserve();
@@ -30,5 +36,38 @@ contract PTMVault is  ERC20, ERC20PresetMinterPauser{
         IERC20(tokenA).transferFrom(msg.sender, swap, amount0);
         IERC20(tokenB).transferFrom(msg.sender, swap, amount1);
 
+
+        uint tokenToMint = amount0 * amount1;
+
+        mint(address(this), tokenToMint);
     }
+
+    function removeLiquidity(uint _amount){
+
+        (uint reserve1, uint resreve2) = ptmSwap.getReserve();
+
+        uint amount0 = _amount * reserve1 / totalSupply();
+        uint amount1 = _amount * resreve2 / totalSupply();
+
+
+        transferFrom(msg.sender, address(this), _amount);
+        IERC20(tokenA).transferFrom(swap, msg.sender, amount0);
+        IERC20(tokenB).transferFrom(swap, msg.sender, amount1);
+
+        burn(_amount);
+    }
+
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+    
 }
